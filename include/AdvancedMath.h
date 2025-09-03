a5542ae8a48fd30f882b9786aef7b05e581f4966
AbhiSwamiUConn <abhiswami2005@gmail.com>
2025-09-02 21:04:17 -0400

    First commit

---
+++ include/AdvancedMath.h
@@ -0,0 +1,10 @@
+#ifndef ADVANCEDMATH_H
+#define ADVANCEDMATH_H
+
+#include "MathUtils.h"
+namespace AdvancedMath{
+    int square(int n);
+}
+
+#endif
+
---
+++ src/AdvancedMath.cpp
@@ -0,0 +1,7 @@
+#include "AdvancedMath.h"
+#include "MathUtils.h"
+namespace AdvancedMath{
+	int square(int n){
+		return MathUtils::multiply(n, n);
+	}
+}
